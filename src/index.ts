41aa906c08cd02ac6627912436b4845c96df9a63
ibuki2003 <main@fuwa.dev>
2020-08-26 10:25:19 +0900

    feat: add index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// export some contents
+
+// apis
+export { default as api } from "./apis/$api";
+export { default } from "./apis/$api";
+
+// types
+export * from "./apis/@types";
+export * from "./apis/exh/status/@types";
+export * from "./apis/reservation/@types";
+export * from "./apis/blog/articles/@types";
+export * from "./apis/general/guest/@types";
+export * from "./apis/blog/revisions/@types";
+export * from "./apis/blog/categories/@types";
+export * from "./apis/blog/revisions/contrib/@types";
+export * from "./apis/general/guest/_id@string/log/@types";
