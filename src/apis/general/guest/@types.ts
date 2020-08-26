06d165ef84baaf1e871f89abc6d3ad86914fac00
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-08-26 18:46:26 +0900

    feat: remove "type" property and add color, term id in guest

--- src/apis/general/guest/@types.ts
+++ src/apis/general/guest/@types.ts
@@ -1,16 +1,11 @@
 export interface Guest {
   id: string;
-  type: Wristband;
+  color_id: string;
+  term_id: string;
   entered_at: string;
   exit_scheduled_time: string | null;
   exited_at: string | null;
   exh_id: string | null;
 }
 
-export type GuestSummary = Pick<Guest, "id" | "type">;
-
-export interface Wristband {
-  color_id: string;
-  color_hex: string;
-  term_id: string;
-}
+export type GuestSummary = Pick<Guest, "id" | "color_id" | "term_id">;
