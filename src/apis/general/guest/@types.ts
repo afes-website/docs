637a335367e27d92d02f6fa592bcbd9132dfce7f
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-08-26 18:59:45 +0900

    feat: set enum in color_id

--- src/apis/general/guest/@types.ts
+++ src/apis/general/guest/@types.ts
@@ -1,6 +1,20 @@
+const colors = [
+  "blue",
+  "red",
+  "yellow",
+  "violet",
+  "orange",
+  "green",
+  "gray",
+  "white",
+  "test_blue",
+  "test_red",
+  "test_yellow",
+] as const;
+
 export interface Guest {
   id: string;
-  color_id: string;
+  color_id: typeof colors[number];
   term_id: string;
   entered_at: string;
   exit_scheduled_time: string | null;
