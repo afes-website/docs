abb2ccad30ab2e2007cedd9f10955adcfd57b642
subaru <s@su8ru.dev>
2020-08-26 17:53:36 +0900

    style: restyling table

--- src/apis/exh/enter/index.ts
+++ src/apis/exh/enter/index.ts
@@ -11,13 +11,13 @@ export interface Methods {
    *
    * @throws Error
    * 400: status code とエラー内容が下記の通り対応
-   * | Status Code | Explanation |
-   * | :----------|  :-- |
-   * | `GUEST_NOT_FOUND` | 該当する Guest が存在しない |
+   * | Status Code             | Explanation                                  |
+   * | :---------------------- | :------------------------------------------- |
+   * | `GUEST_NOT_FOUND`       | 該当する Guest が存在しない                  |
    * | `GUEST_ALREADY_ENTERED` | この展示にすでに入室済みでまだ退室していない |
-   * | `PEOPLE_LIMIT_EXCEEDED` | 人数制限に達した |
-   * | `GUEST_ALREADY_EXITED` | すでに退場済みである |
-   * | `EXIT_TIME_EXCEEDED` | 退場予定時刻を過ぎている |
+   * | `PEOPLE_LIMIT_EXCEEDED` | 人数制限に達した                             |
+   * | `GUEST_ALREADY_EXITED`  | すでに退場済みである                         |
+   * | `EXIT_TIME_EXCEEDED`    | 退場予定時刻を過ぎている                     |
    *
    * @returns 展示の滞在者数に関する情報
    */
--- src/apis/general/enter/index.ts
+++ src/apis/general/enter/index.ts
@@ -15,14 +15,14 @@ export interface Methods {
    *
    * @throws Error
    * 400: Status Code とエラー内容が下記の通り対応
-   * | Status Code | Explanation |
-   * | :-- | :-- |
-   * | `INVALID_WRISTBAND_CODE` | リストバンド ID の形式が正しくない |
-   * | `ALREADY_USED_WRISTBAND` | そのリストバンドは入場処理済みである |
-   * | `RESERVATION_NOT_FOUND` | 該当する予約情報が存在しない |
-   * | `ALREADY_ENTERED_RESERVATION` | 予約者はすでに入場済みである |
-   * | `OUT_OF_RESERVATION_TIME` | 予約者は入場可能時間外にある |
-   * | `WRONG_WRISTBAND_COLOR` | リストバンドの色と退場予定時間が一致しない |
+   * | Status Code                   | Explanation                                |
+   * | :---------------------------- | :----------------------------------------- |
+   * | `INVALID_WRISTBAND_CODE`      | リストバンド ID の形式が正しくない         |
+   * | `ALREADY_USED_WRISTBAND`      | そのリストバンドは入場処理済みである       |
+   * | `RESERVATION_NOT_FOUND`       | 該当する予約情報が存在しない               |
+   * | `ALREADY_ENTERED_RESERVATION` | 予約者はすでに入場済みである               |
+   * | `OUT_OF_RESERVATION_TIME`     | 予約者は入場可能時間外にある               |
+   * | `WRONG_WRISTBAND_COLOR`       | リストバンドの色と退場予定時間が一致しない |
    *
    * @returns 退場時刻
    * 予約情報を参照して設定
