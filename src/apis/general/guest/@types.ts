e652a1b4e415589874065f13b8f181ec4dd5b0b3
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2020-08-26 14:06:46 +0900

    feat: use term instead of time_id, color

--- src/apis/general/guest/@types.ts
+++ src/apis/general/guest/@types.ts
@@ -1,16 +1,16 @@
 export interface Guest {
   id: string;
-  color: Wristband;
+  type: Wristband;
   entered_at: string;
   exit_scheduled_time: string | null;
   exited_at: string | null;
   exh_id: string | null;
 }
 
-export type GuestSummary = Pick<Guest, "id" | "color">;
+export type GuestSummary = Pick<Guest, "id" | "type">;
 
 export interface Wristband {
-  id: string;
-  name: string;
-  hex: string;
+  color_id: string;
+  color_hex: string;
+  term_id: string;
 }
