1300ae726e02db82bd0ff17fef596fbc90be19b1
subaru <s@su8ru.dev>
2020-08-27 15:53:05 +0900

    feat: separate Term interface

--- src/apis/general/term/@types.ts
+++ src/apis/general/term/@types.ts
@@ -1,7 +1,9 @@
+export interface Term {
+  enter_scheduled_time: string;
+  exit_scheduled_time: string;
+  color_id: string;
+}
+
 export interface Terms {
-  [term_id: string]: {
-    enter_scheduled_time: string;
-    exit_scheduled_time: string;
-    color_id: string;
-  }
+  [term_id: string]: Term;
 }
