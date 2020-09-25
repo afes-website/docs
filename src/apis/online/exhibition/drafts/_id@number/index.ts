e39b2a12fe071c8de9faede0bc8a5484d1cada2b
subaru <s@su8ru.dev>
2020-09-26 04:58:01 +0900

    fix: remove draft edit patch

--- src/apis/online/exhibition/drafts/_id@number/index.ts
+++ src/apis/online/exhibition/drafts/_id@number/index.ts
@@ -20,24 +20,4 @@ export interface Methods {
     reqHeaders: AuthToken;
     resBody: Draft;
   };
-
-  /**
-   * draft を編集する
-   *
-   * @remarks
-   * 必要な権限:
-   * - exhibition : 自身が作成した draft の場合のみ編集可能
-   * - blogAdmin : 全ての場合において編集可能
-   *
-   * @throws Error
-   * 403: 該当する draft の閲覧権限がない
-   * 404: 該当する draft が存在しない
-   *
-   * @returns draft の情報
-   */
-  patch: {
-    reqHeaders: AuthToken;
-    reqBody: Pick<Draft, keyof NewDraft | "id">;
-    resBody: Draft;
-  };
 }
