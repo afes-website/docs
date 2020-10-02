af43306edd491cef40252439a0ab60e55c47dc5b
subaru <s@su8ru.dev>
2020-10-02 11:47:18 +0900

    feat: clearly indicate orig or w/h

--- src/apis/images/_id@string/@types.ts
+++ src/apis/images/_id@string/@types.ts
@@ -1,8 +1,11 @@
-export interface ImageParameters {
-  // image の幅
-  w?: string;
-  // image の高さ
-  h?: string;
-  // オリジナルの画像を取得するかどうか
-  orig?: "yes";
-}
+export type ImageParameters =
+  | {
+      // image の幅
+      w?: string;
+      // image の高さ
+      h?: string;
+    }
+  | {
+      // オリジナルの画像を取得するかどうか
+      orig?: "yes";
+    };
