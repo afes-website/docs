4427d3ed414ea5696908c28bfbd873772eb327c0
subaru <s@su8ru.dev>
2020-10-05 09:57:51 +0900

    feat: add Exhibition.type: ExhibitionType

--- src/apis/online/exhibition/@types.ts
+++ src/apis/online/exhibition/@types.ts
@@ -1,9 +1,12 @@
 export interface Exhibition {
   id: string;
   name: string;
+  type: ExhibitionType;
   thumbnail_image_id: string;
   content: string;
   updated_at: string;
 }
+
+export type ExhibitionType = "normal" | "frontier" | "stage";
 
 export type ExhibitionSummary = Pick<Exhibition, "id" | "name">;
