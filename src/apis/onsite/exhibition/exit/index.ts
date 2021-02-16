23e5ffae5eddc0cd89f68bce31cd74b573bd6b9e
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-02-16 17:25:25 +0900

    style: unify table width

--- src/apis/onsite/exhibition/exit/index.ts
+++ src/apis/onsite/exhibition/exit/index.ts
@@ -12,10 +12,10 @@ export interface Methods {
    * @throws Error
    * 400: response Body に必ず status_code が 1 つ含まれる。複数該当する場合はどれか 1 つが返される。
    * 対応表は以下
-   * | status_code | Explanation |
-   * | :-- | :-- |
-   * | `GUEST_NOT_FOUND` | 該当する Guest が存在しない |
-   * | `GUEST_ALREADY_EXITED` | すでに退場済みである |
+   * | status_code            | Explanation                 |
+   * | :--------------------- | :-------------------------- |
+   * | `GUEST_NOT_FOUND`      | 該当する Guest が存在しない |
+   * | `GUEST_ALREADY_EXITED` | すでに退場済みである        |
    *
    * @returns 来場者に関する情報
    */
