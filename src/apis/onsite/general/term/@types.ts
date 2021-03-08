3a049e4ff64067286cca7ffa3ab8d91c998c54b6
subaru <s@su8ru.dev>
2021-03-08 14:02:03 +0900

    feat: replace prefix with guest type id

--- src/apis/onsite/general/term/@types.ts
+++ src/apis/onsite/general/term/@types.ts
@@ -1,18 +1,15 @@
-export const guestTypeList = [
-  "GB", // Guest Blue   (あお)
-  "GR", // Guest Red    (あか)
-  "GY", // Guest Yellow (きいろ)
-  "GG", // Guest Green  (みどり)
-  "GP", // Guest Purple (むらさき)
-  "GO", // Guest Orange (オレンジ)
-  "GW", // Guest White  (紛失用・予備)
-  "SG", // Student Gray (在校生)
-  "TB", // Test  Blue   (テスト青)
-  "TR", // Test  Red    (テスト赤)
-  "TY", // Test  Yellow (テスト黄)
-] as const;
-
-export type GuestType = typeof guestTypeList[number];
+export type GuestType =
+  | "GuestBlue"
+  | "GuestRed"
+  | "GuestYellow"
+  | "GuestGreen"
+  | "GuestPurple"
+  | "GuestOrange"
+  | "GuestWhite"
+  | "StudentGray"
+  | "TestBlue"
+  | "TestRed"
+  | "TestYellow";
 
 export interface Term {
   id: string;
