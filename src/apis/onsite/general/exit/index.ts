34354253f68098a2694fb406f98d71ca3847e4e4
subaru <s@su8ru.dev>
2021-03-13 16:33:35 +0900

    fix: response status code at general/exit

--- src/apis/onsite/general/exit/index.ts
+++ src/apis/onsite/general/exit/index.ts
@@ -10,8 +10,12 @@ export interface Methods {
    * - general
    *
    * @throws Error
-   * 404: ID に該当する Guest が存在しない
-   * 409: 該当する Guest はすでに退場済み
+   * 400: response Body に必ず status_code が 1 つ含まれる。複数該当する場合はどれか 1 つが返される。
+   * 対応表は以下
+   * | status_code            | Explanation                 |
+   * | :--------------------- | :-------------------------- |
+   * | `GUEST_NOT_FOUND`      | 該当する Guest が存在しない |
+   * | `GUEST_ALREADY_EXITED` | すでに退場済みである        |
    *
    * @returns 来場者に関する情報
    */
