8c676b645ae64f28dd42c6fc53945f213b760a7b
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-03-31 21:18:37 +0900

    chore: get rid of blog/online imports

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,6 @@ export { default } from "./apis/$api";
 // types
 export * from "./apis/@types";
 export * from "./apis/auth/@types";
-export * from "./apis/blog/articles/@types";
-export * from "./apis/blog/revisions/@types";
-export * from "./apis/blog/categories/@types";
-export * from "./apis/blog/revisions/contrib/@types";
-export * from "./apis/online/exhibition/@types";
-export * from "./apis/online/drafts/@types";
 export * from "./apis/onsite/exhibition/status/@types";
 export * from "./apis/onsite/general/term/@types";
 export * from "./apis/onsite/general/guest/@types";
