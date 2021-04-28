523e37119f5e665e7b4c439e9d8720a65dc10c33
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-04-28 23:10:05 +0900

    docs: guest/enter,exit remarks

--- src/apis/guests/_id@string/exit/index.ts
+++ src/apis/guests/_id@string/exit/index.ts
@@ -7,22 +7,18 @@ export interface Methods {
    *
    * @remarks
    * 必要な権限:
-   * - exhibition
-   * - admin
-   *
-   * @remarks
-   * admin権限がない場合 exhibition_id には実行ユーザーの Id を指定しなければならない
+   * - exhibition: 自展示の操作のみ許される
+   * - admin: 存在する全展示への操作が許される
    *
    * @throws Error
    * 404: ID に該当する Guest が存在しない
+   * 403: 許可されていない操作
    * 400: response Body に必ず status_code が 1 つ含まれる。複数該当する場合はどれか 1 つが返される。
    * 対応表は以下
    * | status_code            | Explanation                 |
    * | :--------------------- | :-------------------------- |
    * | `GUEST_ALREADY_EXITED` | すでに退場済みである        |
    * | `EXHIBITION_NOT_FOUND` | 該当する展示が存在しない    |
-   *
-   * 403: これを実行する権限がない または 自身以外の exhibition_id を指定する権限がない
    *
    * @returns 来場者に関する情報
    */
