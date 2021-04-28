3002cfd2a3b971cf8963321fbfc24f4cde6151c6
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-04-28 15:29:23 +0900

    feat: allow admin to specify other exhibition

--- src/apis/guests/_id@string/enter/index.ts
+++ src/apis/guests/_id@string/enter/index.ts
@@ -8,6 +8,10 @@ export interface Methods {
    * @remarks
    * 必要な権限:
    * - exhibition
+   * - admin
+   *
+   * * @remarks
+   * admin権限がない場合 exhibition_id には実行ユーザーの Id を指定しなければならない
    *
    * @throws Error
    * 404: ID に該当する Guest が存在しない
@@ -20,6 +24,8 @@ export interface Methods {
    * | `GUEST_ALREADY_EXITED`    | すでに退場済みである                         |
    * | `EXIT_TIME_EXCEEDED`      | 退場予定時刻を過ぎている                     |
    * | `EXHIBITION_NOT_FOUND`    | 該当する展示が存在しない                     |
+   *
+   * 403: これを実行する権限がない または 自身以外の exhibition_id を指定する権限がない
    *
    * @returns 来場者に関する情報
    */
