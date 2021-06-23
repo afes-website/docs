6f5b230039c4de275738fdcecb2ef3de9563c534
subaru <s@su8ru.dev>
2021-06-23 15:28:34 +0900

    feat: add number of member to reservation

--- src/apis/reservations/@types.ts
+++ src/apis/reservations/@types.ts
@@ -3,6 +3,8 @@ import { Term } from "../terms/@types";
 export interface Reservation {
   id: string;
   term: Term;
+  member_all: number;
+  member_checked_in: number;
 }
 
 export interface ReservationQuery {
