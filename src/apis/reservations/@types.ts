f0288c569f13e034501e9e6459b2188f3d3b52a2
subaru <s@su8ru.dev>
2021-06-23 15:44:02 +0900

    docs: add comments for reservation members

--- src/apis/reservations/@types.ts
+++ src/apis/reservations/@types.ts
@@ -3,7 +3,9 @@ import { Term } from "../terms/@types";
 export interface Reservation {
   id: string;
   term: Term;
+  // 予約人数
   member_all: number;
+  // すでに入場済みの人数
   member_checked_in: number;
 }
 
