477ef69bd0722e81b832cd9b4ecffb094071ba3f
subaru <s@su8ru.dev>
2021-06-28 08:50:04 +0900

    feat: add size query

--- src/apis/images/_id@string/index.ts
+++ src/apis/images/_id@string/index.ts
@@ -2,6 +2,10 @@ export interface Methods {
   /**
    * ID から image を取得する
    *
+   * @param size
+   * - `s`: 長辺を 400px に縮小
+   * - `m`: 長辺を 1440px に縮小
+   *
    * @throws Error
    * 404: 指定された image が存在しない
    *
@@ -9,6 +13,9 @@ export interface Methods {
    * 指定された image
    */
   get: {
+    query?: {
+      size?: "s" | "m";
+    };
     resBody: File;
   };
 }
