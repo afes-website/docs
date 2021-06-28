d16a0dc6bf93085c0dcc1c36789f0c697c08189d
subaru <s@su8ru.dev>
2021-06-28 10:42:49 +0900

    docs: delete size description

--- src/apis/images/_id@string/index.ts
+++ src/apis/images/_id@string/index.ts
@@ -2,10 +2,6 @@ export interface Methods {
   /**
    * ID から image を取得する
    *
-   * @param size
-   * - `s`: 長辺を 400px に縮小
-   * - `m`: 長辺を 1440px に縮小
-   *
    * @throws Error
    * 404: 指定された image が存在しない
    *
