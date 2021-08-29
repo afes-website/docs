e0c9442bd3f79f7a155b98cf402b34a587a99a6b
subaru <s@su8ru.dev>
2021-08-29 21:56:36 +0900

    feat: rename /auth/change_password -> /auth/users/_id/change_password

--- src/apis/auth/users/_id@string/change_password/index.ts
+++ src/apis/auth/users/_id@string/change_password/index.ts
@@ -1,11 +1,18 @@
-import { AuthToken } from "../../@types";
+import { AuthToken } from "../../../../@types";
 
 export interface Methods {
   /**
    * パスワード変更
    *
+   * @remarks
+   * 必要な権限:
+   * - (none): 自分のユーザーのみ指定可能
+   * - admin: 全ユーザーを指定可能
+   *
    * @throws Error
    * 400: 新しいパスワードが条件を満たしていない
+   * 403: 許可されていない操作
+   * 404: ID に該当する User が存在しない
    *
    * @returns 成功 返り値なし
    */
